2d4f8cbc0b4edf6f94ae564387b39a0f6e9b676d
yourfired12446 <trololol12446@gmail.com>
2021-10-17 15:31:45 -0400

    a

---
+++ 1.05.ino
@@ -0,0 +1,22 @@
+#define GREEN 3
+#define BLUE 5
+#define RED 6
+#define delayTime 20
+
+void setup() {
+
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(RED, HIGH);
+}
+
+int redVal;
+int blueVal;
+int greenVal;
+
+void loop() {
+    
+}
