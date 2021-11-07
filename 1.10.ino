518803eef5dba2811fdf4e2d77b5b9884509b6a9
yourfired12446 <trololol12446@gmail.com>
2021-11-07 15:07:31 -0500

    a

---
+++ 1.10.ino
@@ -0,0 +1,27 @@
+int potentPin = A3, potentRead, voltage;
+int counter = 0;
+void setup() {
+  pinMode(potentPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  potentRead = analogRead(potentPin);
+
+  voltage = map(potentRead, 0, 1023, 0, 5);
+
+  Serial.print("Potentiometer value: ");
+  Serial.print(potentRead);
+  Serial.print(", Approx. Voltage: ");
+  Serial.println(voltage);
+
+  digitalWrite(counter, HIGH);
+  digitalWrite((counter - 1), HIGH);
+
+  counter++;
+  if (counter == 6;)
+  {
+    counter = 0;
+  }
+  delay(voltage * 150);	
+}
