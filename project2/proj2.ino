9877cb7c4b677b434771c1dfcb1ea2c519ca9f13
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2021-11-14 22:34:29 -0500

    a

--- project2/proj2.ino
+++ project2/proj2.ino
@@ -11,15 +11,31 @@ void setup()
 void loop()
 {
 
- if (digitalRead(12)== HIGH) {
+    // this whole section is responsiple for checking if the button is pressed and then incrementing the variable if it is.
+    // it also does bounds checking
+    if (digitalRead(12) == HIGH)
+    {
 
- }
+        pintouse++;
+        if (pintouse == 12) // make sure the variable doesnt get too high
+        {
+            pintouse = 9;
+        }
+    }
 
+    // the decision to turn on the light is based on the bool isactive
+    // this makes the switch change the value of the variable
+    if (digitalRead(2) == HIGH)
+    {
+        isactive = true;
+    }
+    else
+    {
+        isactive = false;
+    }
 
     if ((digitalRead(2) == HIGH) == isactive)
     {
-
-
     }
     else
     {
