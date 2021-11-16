6115de7962a4f8c0eefdd8526bde984e7e7ed31a
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2021-11-16 17:27:21 -0500

    a

--- unit1/1-12.ino
+++ unit1/1-12.ino
@@ -1,14 +1,37 @@
-int lights[3]  = {1, 2, 3};
+
+const int slidePin = A3;
+const int micPin = A2;	
+
+const int ledPins[5] = {4, 5, 6, 7, 8};
+
 
 void setup()
 {
-    for (int i = 4; i < 8; i++)
-    {
-        pinMode(i, INPUT);
-    }
+	pinMode(slidePin, INPUT);
+	pinMode(micPin, INPUT);
+	
+
+	for (int i=0; i<5; i++)
+	{
+
+		pinMode(ledPins[i], OUTPUT);
+	}
 }
 
 void loop()
 {
-
+	int maximum = analogRead(slidePin);	
+	int micReading = analogRead(micPin);
+	
+	for (int i=0; i<5; i++)
+	{
+		if (micReading > ((maximum / 5) * (i+1)))
+		{
+			digitalWrite(ledPins[i], HIGH);	
+		}
+		else
+		{
+			digitalWrite(ledPins[i], LOW);
+		}
+	}
 }
