9b3036ff54fd650d1b656e39e31885f253465fdf
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2021-11-30 10:12:44 -0500

    adsvdsfsfadsds

--- proj3/proj3.ino
+++ proj3/proj3.ino
@@ -2,11 +2,12 @@ void setup()
 {
     int numstoinitialize[4]  = { 0, 1, 2, 3 };
     Serial.begin(9600);
-
-    
+    Serial.println("welcome to the multifunction sensor");
 }
-
 void loop()
 {
+ do{
+     // statement block
+ } while (digitalRead(2)==HIGH);
     
 }
