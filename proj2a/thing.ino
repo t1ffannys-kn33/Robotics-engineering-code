ae457c0a3441dfd405e188e3db41e11958f783f8
yourfired12446 <trololol12446@gmail.com>
2022-01-27 10:18:40 -0500

    a

--- proj2a/thing.ino
+++ proj2a/thing.ino
@@ -3,6 +3,7 @@
 #define grnledpin 6
 #define whiteledpin 2
 #define buttonpin 7
+#define slidepotentpin A0
 
 int rgbledvalues[] = {0, 0, 0};
 
@@ -21,20 +22,23 @@ void blink(int howmanytimes) {
 }
 
 
-
 void updateleds(int redvalue, int bluvalue, int grnvalue){
-  analogWrite(redledpin, redvalue);
-  analogWrite(bluledpin, bluvalue);
-  analogWrite(grnledpin, grnvalue);
+  int i = map(analogRead(slidepotentpin),0,1024,0,255);
+  analogWrite(redledpin, min(redvalue,i));
+  analogWrite(bluledpin, min(bluvalue,i));
+  analogWrite(grnledpin, min(grnvalue,i));
 }
 
 void loop(){
     digitalWrite(whiteledpin, HIGH); // make sure the led is off
-    if(digitalRead(buttonpin)==LOW){
-      delay(1);
+    for (;;){
+      if(digitalRead(buttonpin)==HIGH){
+        goto breakout;
+      }
     }
-
-    digitalWrite(whiteledpin, LOW); // make sure the led is off
+    breakout:
+      digitalWrite(whiteledpin, LOW); // make sure the led is off
+    blink(1);
 
     for (int i = 0; i < 255; i++) {
       updateleds(0, 255-i, i); // g to b
@@ -46,14 +50,12 @@ void loop(){
       updateleds(i, 0, 255-i); // b to r
       delay(10);
     }
-    blink(1);
-    
+    blink(3);
+
     for (int i = 0; i < 255; i++) {
       updateleds(255-i, i, 0); // r to g
       delay(10);
     }
-    blink(3);
 
 
-    
 }
