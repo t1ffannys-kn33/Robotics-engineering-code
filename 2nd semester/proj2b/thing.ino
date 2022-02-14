0e5bd7222c4b4680169fc0785dd4bf5903294915
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-02-14 09:06:38 -0500

    a

--- 2nd semester/proj2b/thing.ino
+++ 2nd semester/proj2b/thing.ino
@@ -1,6 +1,10 @@
+String questions[3] = {"Swift won her first grammy for which song?",
+"Swifts cats last name starting with Meredith","Swifts alma mater"}
+String answers[3] = {"White Horse","Grey","Notre Dame"}
+
 void setup()
 {
-  
+  Serial.begin(9600);
 }
 
 void loop()
