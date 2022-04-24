5dcdd515c590abfd2c20a1efdef2727ea0e395e9
yourfired12446 <trololol12446@gmail.com>
2022-04-24 17:46:36 -0400

    proj C

---
+++ new era/unit3/projects/proj 3.C/thing.ino
@@ -0,0 +1,36 @@
+// Tomas Turbek
+// Proj 3C
+// 4/24/22
+#include <LiquidCrystal.h> 
+#include <Keypad.h>
+const char secret[5] = "1337";
+
+char keypadlayout[4][4] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'#','0','*','D'}
+};
+
+byte rowpins[ROWS] = {12, 11, 10, 9}; 
+byte colpins[COLS] = {8, 7, 6, 5}; 
+
+Keypad customKeypad = Keypad(makeKeymap(keypadlayout), rowpins, colpins, 4, 4); 
+// thats all the stuff for the keypad, time for the LCD
+
+const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+
+
+
+
+void setup() {
+    lcd.begin(16, 2); 
+    lcd.setCursor(0, 0);
+    lcd.print("guessing game");
+    
+}
+
+void loop(){ 
+
+}
