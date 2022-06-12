d9b8cf226dcc0e6726f93ddbe0428532bd9537e8
t1ffannys-kn33 <trololol12446@gmail.com>
2022-06-12 13:15:40 -0400

    a

--- new era/unit 2/tutorials/tut2.5/1.ino
+++ new era/unit 2/tutorials/tut2.5/1.ino
@@ -1,9 +1,23 @@
-//1.ino
+int potentPin = A3, potentRead, colorindex;
+
+// an array of characters is styled as a string
+char rainbow[] = "ROYGBIV";
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(potentPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //set potentRead to values inputted from potentiometer
+  potentRead = analogRead(potentPin);
+
+  //convert potentiometer reading range 0-1023 to available indexes from rainbow.
+  colorindex = map(potentRead, 0, 1023, 0, 6);
+
+  //prints the raw values and the color with index mapped to potentiometer
+  Serial.print("Potentiometer value: ");
+  Serial.print(potentRead);
+  Serial.print(", Color: ");
+  Serial.println(rainbow[colorindex]);
 }
--- new era/unit 2/tutorials/tut2.5/2.ino
+++ new era/unit 2/tutorials/tut2.5/2.ino
@@ -1,9 +1,25 @@
-//1.ino
+int potentPin = A3, potentRead, colorindex, ledbrightness;
+
+// an array of characters is styled as a string
+char rainbow[] = "ROYGBIV";
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(potentPin, INPUT);
+  pinMode(p)
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //set potentRead to values inputted from potentiometer
+  potentRead = analogRead(potentPin);
+
+  //convert potentiometer reading range 0-1023 to available indexes from rainbow.
+  colorindex = map(potentRead, 0, 1023, 0, 6);
+  ledbrightness = map(potentRead, 0, 1023, 0, 255);
+  //prints the raw values and the color with index mapped to potentiometer
+  analogWrite 
+  Serial.print("Potentiometer value: ");
+  Serial.print(potentRead);
+  Serial.print(", Color: ");
+  Serial.println(rainbow[colorindex]);
 }
